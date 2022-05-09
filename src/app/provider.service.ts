ed7266b6b26d48afc080485617b3157f1cc738b1
madhu <bamadhu1995@gmail.com>
2022-05-09 22:30:53 +0530

    adding input keystore

--- src/app/provider.service.ts
+++ src/app/provider.service.ts
@@ -6,7 +6,14 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ProviderService {
 
+  private baseURL =  'https://gist.githubusercontent.com/manzooralam/4b6aca6e1aa794c57c0cb6e7a2605109/raw/694828a0b1084ef63ff507ed85de3db6e3931e99/list.json';
+         
+
   constructor(private http: HttpClient) { }
+
+  getData(){
+    return this.http.get(this.baseURL)
+  }
 
   userInfo()
   {
@@ -20,3 +27,4 @@ export class ProviderService {
         });
     });
   }}
+  
